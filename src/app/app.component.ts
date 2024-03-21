46bb293a5398491088eb7b3d04889af97710873f
ricardosantanadev <ricardosantanadev@gmail.com>
2024-03-21 18:32:38 -0300

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'example-simple-angular-table-json-local-contendo-um-array-com-um-objeto-httpclient';
+}
